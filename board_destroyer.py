9a198aad5a46d3cfd2c1a8a23b5bb2b73c8635de
Arslonbek25 <amadyarov25@gmail.com>
2023-05-18 23:37:42 +0500

    Refactor

--- board_destroyer.py
+++ board_destroyer.py
@@ -3,26 +3,54 @@ import numpy as np
 from imutils.object_detection import non_max_suppression
 
 img_board = cv2.imread("assets/cb.png")
-img_piece = cv2.imread("assets/black_bishop.png", cv2.IMREAD_UNCHANGED)
-
 img_board_gray = cv2.cvtColor(img_board, cv2.COLOR_BGR2GRAY)
-img_piece_gray = cv2.cvtColor(img_piece, cv2.COLOR_BGR2GRAY)
 
-h, w = img_piece_gray.shape
-mask = img_piece[:, :, 3]
+piece_names = {
+    # "white_king": "K",
+    # "white_queen": "Q",
+    # "white_rook": "R",
+    # "white_bishop": "B",
+    # "white_knight": "N",
+    # "white_pawn": "P",
+    "black_king": "k",
+    "black_queen": "q",
+    "black_rook": "r",
+    "black_bishop": "b",
+    "black_knight": "n",
+    "black_pawn": "p",
+}
 
-res = cv2.matchTemplate(img_board_gray, img_piece_gray, cv2.TM_SQDIFF_NORMED, mask=mask)
-threshold = 0.1
-loc = np.where(res <= threshold)
 
-rects = []
-for x, y in zip(*loc[::-1]):
-    rects.append((x, y, x + w, y + h))
+def find_piece(board, piece):
+    img_piece = cv2.imread(f"assets/{piece}.png", cv2.IMREAD_UNCHANGED)
+    img_piece_gray = cv2.cvtColor(img_piece, cv2.COLOR_BGR2GRAY)
 
-pick = non_max_suppression(np.array(rects))
-for r in pick:
-    x, y, h, w = r
-    cv2.rectangle(img_board, (x, y), (h, w), (0, 0, 255), 5)
+    h, w = img_piece_gray.shape
+    mask = img_piece[:, :, 3]
 
-cv2.imshow("Chess", img_board)
+    res = cv2.matchTemplate(board, img_piece_gray, cv2.TM_SQDIFF_NORMED, mask=mask)
+    loc = np.where(res <= 0.1)
+
+    rects = [(x, y, x + w, y + h) for x, y in zip(*loc[::-1])]
+
+    return non_max_suppression(np.array(rects))
+
+
+# Highlight all pieces
+# for piece_name in piece_names.keys():
+#     print(piece_name)
+#     pieces = find_piece(img_board_gray, piece_name)
+#     for r in pieces:
+#         x, y, h, w = r
+#         cv2.rectangle(img_board, (x, y), (h, w), (0, 0, 255), 5)
+
+# Testing one piece
+# pieces = find_piece(img_board_gray, "black_pawn")
+# for r in pieces:
+#     x, y, h, w = r
+#     cv2.rectangle(img_board, (x, y), (h, w), (0, 0, 255), 5)
+
+cv2.imshow("Chess", img_board_gray)
+cv2.waitKey()
+cv2.imshow("Chess", cv2.imread("assets/white_pawn.png", 0))
 cv2.waitKey()
