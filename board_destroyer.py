566b7f1c651c40a5b48213e138c172ae6287cabf
Arslonbek25 <amadyarov25@gmail.com>
2023-06-03 17:47:31 +0500

    SAN to coords conversion

--- board_destroyer.py
+++ board_destroyer.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 from imutils.object_detection import non_max_suppression
+from engine import get_best_move
 
 # TODO: save images in memory after loading them to save computing time
 
 # Load and resize the image
-img_board = cv2.imread("assets/cb.png")
+img_board = cv2.imread("assets/cb4.png")
 resize_factor = 800 / (sum(img_board.shape[:2]) / 2)
 img_board = cv2.resize(img_board, (0, 0), fx=resize_factor, fy=resize_factor)
 
@@ -14,7 +15,7 @@ img_board_gray = cv2.cvtColor(img_board, cv2.COLOR_BGR2GRAY)
 
 # Calculate the square size
 bh, bw = img_board_gray.shape
-sq_size = int((bh + bw) / 16 * 0.98)
+sq_size = int((bh + bw) / 16 * 0.97)
 
 piece_names = {
     "white_king": "K",
@@ -80,13 +81,29 @@ def get_fen(coords):
     return "{} {}".format(fen.rstrip("/"), turn)
 
 
+cv2.imshow("Chess", img_board)
+cv2.waitKey()
+
 turn = None
 while turn not in ["b", "w"]:
     turn = input("Whose turn is it? (b/w): ")
 
 
 fen = get_fen(coords)
-print(fen)
 
-cv2.imshow("Chess", img_board)
-cv2.waitKey()
+best_move = get_best_move(fen)
+
+
+def san_to_coords(san):
+    ranks = "abcdefgh"
+    files = "12345678"
+    x1 = ranks.index(san[0])
+    y1 = 7 - files.index(san[1])
+    x2 = ranks.index(san[2])
+    y2 = 7 - files.index(san[3])
+    return [x1, y1, x2, y2]
+
+
+move_coords = san_to_coords(str(best_move.move))
+move_coords = list(map(lambda z: z * sq_size, move_coords))
+print(move_coords)
---
+++ engine.py
@@ -0,0 +1,14 @@
+import chess
+import chess.engine
+
+engine_path = "/usr/local/bin/stockfish"
+engine = chess.engine.SimpleEngine.popen_uci(engine_path)
+engine.configure({"Skill Level": 12})
+
+
+def get_best_move(fen):
+    board = chess.Board(fen)
+    print(board)
+    result = engine.play(board, chess.engine.Limit(time=.5))
+    engine.quit()
+    return result
